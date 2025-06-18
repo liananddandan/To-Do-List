9402df2df76432b73d8333b2db77400b9bd88c46
Chen Li <cl649@students.waikato.ac.nz>
2025-06-18 19:44:27 +1200

    use environment variables in all DbContext factories to avoid hardcoded secrets

--- To-Do-List/Identity/DesignFactory/MyIdentityDbContextFactory.cs
+++ To-Do-List/Identity/DesignFactory/MyIdentityDbContextFactory.cs
@@ -1,21 +1,21 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
-using To_Do_List.Configuration.DbContext;
+using To_Do_List.Identity.DbContext;
 
-namespace To_Do_List.Configuration.DesignFactory;
+namespace To_Do_List.Identity.DesignFactory;
 
-public class SystemConfigurationContextFactory : IDesignTimeDbContextFactory<SystemConfigurationContext>
+public class MyIdentityDbContextFactory : IDesignTimeDbContextFactory<MyIdentityDbContext>
 {
-    public SystemConfigurationContext CreateDbContext(string[] args)
+    public MyIdentityDbContext CreateDbContext(string[] args)
     {
         var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection");
 
         if (string.IsNullOrWhiteSpace(connectionString))
             throw new InvalidOperationException("Environment variable 'ConnectionStrings__DefaultConnection' is not set.");
 
-        var optionsBuilder = new DbContextOptionsBuilder<SystemConfigurationContext>();
+        var optionsBuilder = new DbContextOptionsBuilder<MyIdentityDbContext>();
         optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
 
-        return new SystemConfigurationContext(optionsBuilder.Options);
+        return new MyIdentityDbContext(optionsBuilder.Options);
     }
 }
--- To-Do-List/Tasks/DesignFactory/TaskDbContextFactory.cs
+++ To-Do-List/Tasks/DesignFactory/TaskDbContextFactory.cs
@@ -1,21 +1,21 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
-using To_Do_List.Configuration.DbContext;
+using To_Do_List.Tasks.DbContext;
 
-namespace To_Do_List.Configuration.DesignFactory;
+namespace To_Do_List.Tasks.DesignFactory;
 
-public class SystemConfigurationContextFactory : IDesignTimeDbContextFactory<SystemConfigurationContext>
+public class TaskDbContextFactory : IDesignTimeDbContextFactory<TaskDbContext>
 {
-    public SystemConfigurationContext CreateDbContext(string[] args)
+    public TaskDbContext CreateDbContext(string[] args)
     {
         var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection");
-
         if (string.IsNullOrWhiteSpace(connectionString))
+        {
             throw new InvalidOperationException("Environment variable 'ConnectionStrings__DefaultConnection' is not set.");
+        }
 
-        var optionsBuilder = new DbContextOptionsBuilder<SystemConfigurationContext>();
+        var optionsBuilder = new DbContextOptionsBuilder<TaskDbContext>();
         optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
-
-        return new SystemConfigurationContext(optionsBuilder.Options);
+        return new TaskDbContext(optionsBuilder.Options);
     }
 }
